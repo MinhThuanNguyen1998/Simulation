8727d675380457364710422efae96ebb0d4d9c85
thuannm <thuannm.ot@gmail.com>
2025-09-18 15:55:28 +0700

    - Update : UI : size of button , text of button, change notification

--- Assets/Master/Scripts/Data/Config.cs
+++ Assets/Master/Scripts/Data/Config.cs
@@ -4,15 +4,15 @@ public class Config : MonoBehaviour
 {
     // Text title
     public const string Text_Hoan_Thanh = "Hoàn thành !!!";
-    public const string Text_Thu_Lai = "Sai thao tác !!!";
-    public const string Text_Doi_Gia_Tri = "Đổi giá trị khác";
-    public const string Text_Chua_Hoan_Thanh = "Chưa hoàn thành";
+    public const string Text_Thu_Lai = "Sai thao tác.\r\n Đọc lại hướng dẫn !!!";
+    public const string Text_Doi_Gia_Tri = "Đổi giá trị khác!!!";
+    public const string Text_Chua_Hoan_Thanh = "Chưa hoàn thành!!!";
 
     // Text button
     public const string Text_OK = "OK";
 
     // Text popup
-    public const string Text_CreateFolder = "1. Chọn vị trí cần tạo thư mục\r\n2. Nhấn chuột phải vào khoảng trống\r\n3. New -> Folder\r\n";
-    public const string Text_RenameFolder = "1. Nhấn chuột phải vào thư mục cần đổi tên\r\n2. Rename\r\n";
-    public const string Text_DeleteFolder = "1. Nhấn chuột phải vào thư mục cần xóa\r\n2. Delete\r\n";
+    public const string Text_CreateFolder = "1.Chọn vị trí cần tạo thư mục\r\n2.Nhấn chuột phải vào khoảng trống\r\n3.New -> Folder\r\n";
+    public const string Text_RenameFolder = "1.Nhấn chuột phải vào thư mục cần đổi tên\r\n2.Rename\r\n";
+    public const string Text_DeleteFolder = "1.Nhấn chuột phải vào thư mục cần xóa\r\n2.Delete\r\n";
 }
